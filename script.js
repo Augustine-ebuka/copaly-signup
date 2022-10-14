160f50c099756e62f0f7e3b87b53f2bf957c9a71
Hebrus <augustineebuka98@gmail.com>
2022-10-14 15:42:57 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,6 @@
+var openBtn = document.getElementsByClassName('open');
+var navMenu = document.getElementsByClassName('open');
+
+openBtn.addEventListener('click', function () {
+   navMenu.style.display = 'block'; 
+});
